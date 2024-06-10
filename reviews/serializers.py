16a7d789fb59c21ec5f4fa0d6b45d200a01c4600
junaaid96 <info.junaidulislam@gmail.com>
2024-06-10 18:59:51 +0600

    review serializers updated

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from .models import Review
 from profiles.serializers import CustomerProfileSerializer
 from services.serializers import ServiceSerializer
+from rest_framework.serializers import StringRelatedField
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     user = CustomerProfileSerializer(read_only=True)
     service = ServiceSerializer(read_only=True)
+    customer = StringRelatedField()
 
     class Meta:
         model = Review
